cc966c839e9c94ed254a9f1d55ebca9263c280d9
dastanchick <dosyafree32@gmail.com>
2022-11-23 17:59:15 +0600

    I added class main dfv

--- src/Main.java
+++ src/Main.java
@@ -5,11 +5,6 @@ public class Main {
      Scanner scanner = new Scanner(System.in);
 
 
-        System.out.println("enter bus code");
-     int index = scanner.nextInt();
-     while (index<0 || index>3){
-         System.err.println("no such bus");
-         index = scanner.nextInt();
-     }
+    
     }
 }
