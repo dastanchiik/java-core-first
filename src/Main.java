a0ea7db87967598e2651f19ff0fb97b6a3a59173
dastanchick <dosyafree32@gmail.com>
2022-11-23 16:45:44 +0600

    I added class main

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,11 @@ public class Main {
     public static void main(String[] args) {
      Scanner scanner = new Scanner(System.in);
 
+        System.out.println("enter bus code");
+     int index = scanner.nextInt();
+     while (index<0 || index>3){
+         System.err.println("no such bus");
+         index = scanner.nextInt();
+     }
     }
 }
