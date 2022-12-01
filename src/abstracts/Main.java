77c35d146fc7da4421cd1fc0718438fd6935eddb
dastanchick <dosyafree32@gmail.com>
2022-12-01 20:11:52 +0600

    I added class

--- src/abstracts/Main.java
+++ src/abstracts/Main.java
@@ -1,4 +1,11 @@
 package abstracts;
 
 public class Main {
+    public static void main(String[] args) {
+        Person2 person2 = new Person2("Dastan",15,1.60,"male");
+        Person3 person3 = new Person3("Kurmangazy",15,1.80,"male");
+        Person3 person31 = new Person3("Altynay",12,1.40,"female");
+        System.out.println(person2+"\n"+person3+"\n"+person31);
+
+    }
 }
--- src/abstracts/Person3.java
+++ src/abstracts/Person3.java
@@ -1,4 +1,16 @@
 package abstracts;
 
-public class Person3 {
+public class Person3 extends Person{
+    private String gender;
+    public Person3(String name, int age, double height,String gender) {
+        super(name, age, height);
+        this.gender = gender;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString()+'\''+
+                "gender='" + gender + '\'' +
+                '}';
+    }
 }
