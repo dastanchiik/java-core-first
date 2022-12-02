c388de0871119b805556635757dc1846a72b49d3
dastanchick <dosyafree32@gmail.com>
2022-12-02 14:40:03 +0600

    I added class and array

--- src/probely/Exercise.java
+++ src/probely/Exercise.java
@@ -1,11 +1,6 @@
 package probely;
-
-import java.util.Scanner;
-
 public class Exercise {
     public static void main(String[] args) {
-
-        Scanner scanner = new Scanner(System.in);
         int[] array = {1,2,3,4,5,6,7,8,9,10};
         for (int i = array.length - 1; i >= 0; i--) {
             System.out.print(array[i]+" ");
