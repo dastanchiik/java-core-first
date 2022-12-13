78a7514e83caf270561e50899a593b2e825b404f
dastanchick <dosyafree32@gmail.com>
2022-12-13 18:40:59 +0600

    I added class hdsbfkb

--- src/Shark.java
+++ src/Shark.java
@@ -1,11 +1,17 @@
-public class Shark extends Animal{
+public class Shark extends Animal implements Flyable{
 
     public Shark(String name, int age, String color) {
         super(name, age, color);
     }
 
+
     @Override
     public String toString() {
         return "Shark:"+super.toString();
+    }
+
+    @Override
+    public void fly() {
+        System.out.println("swimming fast");
     }
 }
