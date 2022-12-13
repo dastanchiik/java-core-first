9787d02f870f227a2ed1d275d4fcfca96edc5d5a
dastanchick <dosyafree32@gmail.com>
2022-12-13 18:26:01 +0600

    I added class hdsbfkb

--- src/Shark.java
+++ src/Shark.java
@@ -1,7 +1,11 @@
 public class Shark extends Animal{
-    private
 
     public Shark(String name, int age, String color) {
         super(name, age, color);
+    }
+
+    @Override
+    public String toString() {
+        return "Shark:"+super.toString();
     }
 }
