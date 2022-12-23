b052bd6dffb1d6c2eb65b76b57b8c2707f856e6f
dastanchick <dosyafree32@gmail.com>
2022-12-23 16:16:00 +0600

    I added class hdsbfkb

--- src/Main.java
+++ src/Main.java
@@ -7,14 +7,6 @@ public class Main {
     method();
     }
     public static void method(){
-        ArrayList<Integer>arr = new ArrayList<>();
-       Scanner scanner = new Scanner(System.in);
-        System.out.println();
-       int[]array = {1,2,3,4,5,6};
-        for (int i = 1; i <= array.length; i++) {
-            System.out.print(i+" ");
-        }
-        System.out.println("\n");
-        System.out.println("max number:"+Arrays.stream(array).max());
+
     }
 }
