5b95f8aae8e39883a3ce18e21ad5d0d8197a4882
dastanchick <dosyafree32@gmail.com>
2022-12-31 00:10:39 +0600

    I added class hdsbfkb

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,10 @@
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
 public class Main {
     public static void main(String[] args) {
-        method();
-    }
 
-    public static void method() {
-        ArrayList<User>users = new ArrayList<>();
-        users.add(new User("c",12));
-        users.add(new User("b",1432));
-        users.add(new User("a",1));
-        Collections.sort(users);
-        for (User e:users) {
-            System.out.println(e);
-        }
     }
 }
