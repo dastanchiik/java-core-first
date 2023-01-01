4bfabd0941191e369146895af3c435784fb9798b
dastanchick <dosyafree32@gmail.com>
2023-01-01 18:46:55 +0600

    I added class

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,11 @@
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.Set;
 
 public class Main {
     public static void main(String[] args) {
-
+        method(new Object[]{3,4,7,9,8,87,67});
+    }
+    public static <T> void method(T[] a){
+        for (T e:a) {
+            System.out.println(e);
+        }
     }
 }
