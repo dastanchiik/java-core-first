56da7da04ebba6bf34cf8088bb735b54ec73a160
dastanchick <dosyafree32@gmail.com>
2023-01-07 23:32:08 +0600

    I'm check

---
+++ src/App.java
@@ -0,0 +1,8 @@
+public class App {
+    public static void main(String[] args) {
+        Wow wow = new Wow("Askar",12);
+        wow.setName("Askarali");
+//        System.out.println(wow.getName());
+        System.out.println(wow.getName());
+    }
+}
