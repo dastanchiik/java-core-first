7c7c937f2006ccd17d4434ebf2c7f12d666745af
dastan <dosyafree32@gmail.com>
2023-01-11 19:56:22 +0600

    I'm commit with new laptop

--- src/App.java
+++
@@ -1,8 +0,0 @@
-public class App {
-    public static void main(String[] args) {
-        Wow wow = new Wow("Askar",12);
-        wow.setName("Askarali");
-//        System.out.println(wow.getName());
-        System.out.println(wow.getName());
-    }
-}
