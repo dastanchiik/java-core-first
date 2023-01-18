c87539b9f2419be712741b6d6fe172a282a7dd17
dastan <dosyafree32@gmail.com>
2023-01-18 19:20:04 +0600

    I'm commit not sql!!!

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,15 @@
 import java.util.LinkedList;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-
+     Scanner scanner = new Scanner(System.in);
+     String name = scanner.next();
+     int a = name.length();
+     int count = 0;
+     while (count != a){
+         System.out.println(name.substring(count));
+         count++;
+     }
     }
 }
