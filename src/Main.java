9b511875df4c7ed7e104d5b2d92f95e845895666
dastan <dosyafree32@gmail.com>
2023-01-19 13:49:20 +0600

    I'm commit not sql!!!

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,14 @@
 import java.util.Scanner;
+import java.util.concurrent.TimeUnit;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
      Scanner scanner = new Scanner(System.in);
      String name = scanner.next();
      int a = name.length();
      int count = 0;
      while (count != a){
+         TimeUnit.SECONDS.sleep(1);
          System.out.println(name.substring(count));
          count++;
      }
