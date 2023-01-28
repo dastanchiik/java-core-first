bc48c41d45d1c54bc1029651ed80992f90a262dd
dastan <dosyafree32@gmail.com>
2023-01-28 20:34:29 +0600

    I'm commit not sql!!!

--- src/Solution.java
+++ src/Solution.java
@@ -1,39 +1,10 @@
 import java.util.Scanner;
 
 public class Solution {
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int a = scanner.nextInt();
         int b = a * a;
-        int c = 0;
-        while (c <= b) {
-                c++;
-            for (int i = 0; i < b; ++i) {
-                for (int j = 0; j < i; ++j){
-                    System.out.print(c);
-                System.out.println();
-                }
-            }
-        }
     }
 }
-//        System.out.println(getNumDigits(10000, 10401));
-//    }
-//
-//    public static int getNumDigits(int numCount, int END) {
-//        int numDigits;
-//
-//        numDigits = 0;
-//
-//        while (numCount <= END) {
-//            numDigits = (int) (Math.log10(numCount) + 1);
-//
-//            if (numCount == 0) {
-//                numDigits = 1;
-//            }
-//
-//            numCount++;
-//
-//        }
-//
-//        return numDigits;
