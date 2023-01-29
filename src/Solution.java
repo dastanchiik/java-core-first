8614279518a71ea62ecd7b9c662cf843d87738b0
dastan <dosyafree32@gmail.com>
2023-01-29 14:48:47 +0600

    I'm cleaned

--- src/Solution.java
+++ src/Solution.java
@@ -1,36 +1,5 @@
-import java.util.LinkedHashMap;
-import java.util.Map;
-
 public class Solution {
     public static void main(String[] args) {
-        System.out.println(intToRoman(6));
-    }
-    public static String intToRoman(int num) {
-        Map<String,Integer> map = new LinkedHashMap<>();
-        StringBuilder sb = new StringBuilder();
-        map.put("M",1000);
-        map.put("CM",900);
-        map.put("D",500);
-        map.put("CD",400);
-        map.put("C",100);
-        map.put("XC",90);
-        map.put("L",50);
-        map.put("XL",40);
-        map.put("X",10);
-        map.put("IX",9);
-        map.put("V",5);
-        map.put("IV",4);
-        map.put("I",1);
-
-        for(Map.Entry<String, Integer> entry : map.entrySet()) {
-
-            while(num >= entry.getValue()) {
-                num = num - entry.getValue();
-                sb.append(entry.getKey());
-            }
-        }
-
-        return sb.toString();
 
     }
 }
