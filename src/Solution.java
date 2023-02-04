3b83434c066462b455b6b6d8fec607c3888b0d95
dastan <dosyafree32@gmail.com>
2023-02-04 19:58:37 +0600

    I don't know what is I'm commit

--- src/Solution.java
+++ src/Solution.java
@@ -1,6 +1,11 @@
+import java.util.Arrays;
 
 public class Solution {
     public static void main(String[] args) {
-
+        System.out.println(highAndLow("1,2,3,4,5"));
+    }
+    public static String highAndLow(String numbers) {
+        int max = numbers.chars().max().getAsInt();
+        return String.valueOf(Character.getNumericValue(max));
     }
 }
