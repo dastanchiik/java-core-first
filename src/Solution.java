608f41b4d023fd329f7597d23cd77b98cb9543a7
dastan <dosyafree32@gmail.com>
2023-02-10 17:17:14 +0600

    I don't know what is I'm commit

--- src/Solution.java
+++ src/Solution.java
@@ -1,27 +1,23 @@
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.regex.*;
 
 public class Solution {
     public static void main(String[] args) {
         String[] str = {"abs", "01", "0001", "12", "512", "a12bs"};
-        StringBuilder s = new StringBuilder();
-        for(int a=Integer.MAX_VALUE; a>0; a/=10)
-            s.insert(0, a%10);
-        System.out.println(s);
-
-//        System.out.println(method(str));
-//    }
-//    public static ArrayList<Integer>method(String[] str){
-//        ArrayList<Integer>list = new ArrayList<>();
-//        StringBuilder a = new StringBuilder();
-//        for (String s : str) {
-//            a.append(s);
-//        }
+        System.out.println(method(str));
+    }
+    public static ArrayList<Integer>method(String[] str){
+        ArrayList<Integer>list = new ArrayList<>();
+        StringBuilder a = new StringBuilder();
+        for (String s : str) {
+            String[]arr = s.split("\\D+");
+            Integer number = Integer.parseInt(String.join("",arr));
+            list.add(number);
+        }
+        return list;
 //        String s1 = String.valueOf(a);
 //        int w = s1.length();
-//        String q = s1.substring(w / 2);
-//        String e = s1.substring(0, w / 2);
+//        String q = s1.substring(w/2);
+//        String e = s1.substring(0,w/2);
 //        String[] s2_array = q.split("\\D+");
 //        String[] s1_array = e.split("\\D+");
 //        Integer numbers = Integer.parseInt(String.join("", s2_array));
@@ -29,7 +25,8 @@ public class Solution {
 //        StringBuilder s = new StringBuilder();
 //        s.append(number);
 //        s.append(numbers);
-//        list.add(Integer.parseInt(String.valueOf(s)));
+//        String v = String.valueOf(s);
+//        list.add(v);
 //        return list;
     }
 }
