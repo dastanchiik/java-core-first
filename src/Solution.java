90cf247edff180302c323704f295883c23d7d28d
dastan <dosyafree32@gmail.com>
2023-02-10 17:17:55 +0600

    I don't know what I'm commit

--- src/Solution.java
+++ src/Solution.java
@@ -14,19 +14,5 @@ public class Solution {
             list.add(number);
         }
         return list;
-//        String s1 = String.valueOf(a);
-//        int w = s1.length();
-//        String q = s1.substring(w/2);
-//        String e = s1.substring(0,w/2);
-//        String[] s2_array = q.split("\\D+");
-//        String[] s1_array = e.split("\\D+");
-//        Integer numbers = Integer.parseInt(String.join("", s2_array));
-//        Integer number = Integer.parseInt(String.join("", s1_array));
-//        StringBuilder s = new StringBuilder();
-//        s.append(number);
-//        s.append(numbers);
-//        String v = String.valueOf(s);
-//        list.add(v);
-//        return list;
     }
 }
