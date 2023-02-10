c89b679cea210e7d7e3b49175c0f07c1e8efdb3a
dastan <dosyafree32@gmail.com>
2023-02-10 17:24:01 +0600

    I don't know what I'm commit

--- src/Solution.java
+++ src/Solution.java
@@ -5,14 +5,14 @@ public class Solution {
         String[] str = {"abs", "01", "0001", "12", "512", "a12bs"};
 //        System.out.println(method(str));
     }
-//    puKKblic static ArrayList<Integer>method(String[] str){
-//        ArrayList<Integer>list = new ArrayList<>();
-//        StringBuilder a = new StringBuilder();
-//        for (String s : str) {
-//            String[]arr = s.split("\\D+");
-//            Integer number = Integer.parseInt(String.join("",arr));
-//            list.add(number);
-//        }
-//        return list;
-//    }
+    public static ArrayList<Integer>method(String[] str){
+        ArrayList<Integer>list = new ArrayList<>();
+        StringBuilder a = new StringBuilder();
+        for (String s : str) {
+            String[]arr = s.split("\\D+");
+            Integer number = Integer.parseInt(String.join("",arr));
+            list.add(number);
+        }
+        return list;
+    }
 }
