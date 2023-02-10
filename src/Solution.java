6715862b294de96361d6db36d9dd143b72de9ce1
dastan <dosyafree32@gmail.com>
2023-02-10 17:24:20 +0600

    I don't know what I'm commit

--- src/Solution.java
+++ src/Solution.java
@@ -3,7 +3,7 @@ import java.util.ArrayList;
 public class Solution {
     public static void main(String[] args) {
         String[] str = {"abs", "01", "0001", "12", "512", "a12bs"};
-//        System.out.println(method(str));
+        System.out.println(method(str));
     }
     public static ArrayList<Integer>method(String[] str){
         ArrayList<Integer>list = new ArrayList<>();
