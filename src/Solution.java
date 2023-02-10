cca4f8f5b00fe6da59727376642908ac447591ad
dastan <dosyafree32@gmail.com>
2023-02-10 17:22:52 +0600

    I don't know what I'm commit

--- src/Solution.java
+++ src/Solution.java
@@ -3,16 +3,16 @@ import java.util.ArrayList;
 public class Solution {
     public static void main(String[] args) {
         String[] str = {"abs", "01", "0001", "12", "512", "a12bs"};
-        System.out.println(method(str));
+//        System.out.println(method(str));
     }
-    public static ArrayList<Integer>method(String[] str){
-        ArrayList<Integer>list = new ArrayList<>();
-        StringBuilder a = new StringBuilder();
-        for (String s : str) {
-            String[]arr = s.split("\\D+");
-            Integer number = Integer.parseInt(String.join("",arr));
-            list.add(number);
-        }
-        return list;
-    }
+//    puKKblic static ArrayList<Integer>method(String[] str){
+//        ArrayList<Integer>list = new ArrayList<>();
+//        StringBuilder a = new StringBuilder();
+//        for (String s : str) {
+//            String[]arr = s.split("\\D+");
+//            Integer number = Integer.parseInt(String.join("",arr));
+//            list.add(number);
+//        }
+//        return list;
+//    }
 }
