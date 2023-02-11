93defb351492fddfcdd25356e8c6ffded9ca6a43
dastan <dosyafree32@gmail.com>
2023-02-11 11:26:05 +0600

    I don't know what I'm commit

--- src/Solution.java
+++ src/Solution.java
@@ -1,18 +1,33 @@
-import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Solution {
+
     public static void main(String[] args) {
-        String[] str = {"abs", "01", "0001", "12", "512", "a12bs"};
-        System.out.println(method(str));
+        int n1 = 366, n2 = 60;
+        int hcf = hcf(n1, n2);
+
+        System.out.printf("G.C.D of %d and %d is %d.", n1, n2, hcf);
     }
-    public static ArrayList<Integer>method(String[] str){
-        ArrayList<Integer>list = new ArrayList<>();
-        StringBuilder a = new StringBuilder();
-        for (String s : str) {
-            String[]arr = s.split("\\D+");
-            Integer number = Integer.parseInt(String.join("",arr));
-            list.add(number);
-        }
-        return list;
+
+    public static int hcf(int n1, int n2)
+    {
+        if (n2 != 0)
+            return hcf(n2, n1 % n2);
+        else
+            return n1;
     }
 }
+//    public static void main(String[] args) {
+//        Scanner scanner = new Scanner(System.in);
+//        int num = scanner.nextInt(), sum = 0;
+//        int count = 0;
+//        while (count<=num) {
+//            count++;
+//            while (num % 2 == 1) {
+//                // sum = sum + i;
+//                System.out.println(num);
+//                sum += num;
+//            }
+//        }
+//        System.out.println("Sum = " + sum);
+//    }
