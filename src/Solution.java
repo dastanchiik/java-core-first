695b883bce324362121047a9d95c4da39dde5347
dastan <dosyafree32@gmail.com>
2023-02-11 12:07:49 +0600

    I don't know what I'm commit

--- src/Solution.java
+++ src/Solution.java
@@ -1,19 +1,18 @@
+import java.util.Random;
 import java.util.Scanner;
 
 public class Solution {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        Random ran = new Random();
         int num = scanner.nextInt(), sum = 0;
-        int count = 0;
-        while (count<=num) {
-            count++;
-            while (num % 2 == 1) {
-                // sum = sum + i;
-                System.out.println(num);
-                sum += num;
+        int count = 0 ;
+        while (count != num){
+            while (count %2==1){
+                count++;
             }
+            count++;
         }
-        System.out.println("Sum = " + sum);
     }
 }
