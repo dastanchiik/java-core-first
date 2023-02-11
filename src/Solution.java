8c1cad7e2f1982d3a8ea550887e68c30325bf2fa
dastan <dosyafree32@gmail.com>
2023-02-11 11:27:40 +0600

    I don't know what I'm commit

--- src/Solution.java
+++ src/Solution.java
@@ -3,31 +3,17 @@ import java.util.Scanner;
 public class Solution {
 
     public static void main(String[] args) {
-        int n1 = 366, n2 = 60;
-        int hcf = hcf(n1, n2);
-
-        System.out.printf("G.C.D of %d and %d is %d.", n1, n2, hcf);
-    }
-
-    public static int hcf(int n1, int n2)
-    {
-        if (n2 != 0)
-            return hcf(n2, n1 % n2);
-        else
-            return n1;
+        Scanner scanner = new Scanner(System.in);
+        int num = scanner.nextInt(), sum = 0;
+        int count = 0;
+        while (count<=num) {
+            count++;
+            while (num % 2 == 1) {
+                // sum = sum + i;
+                System.out.println(num);
+                sum += num;
+            }
+        }
+        System.out.println("Sum = " + sum);
     }
 }
-//    public static void main(String[] args) {
-//        Scanner scanner = new Scanner(System.in);
-//        int num = scanner.nextInt(), sum = 0;
-//        int count = 0;
-//        while (count<=num) {
-//            count++;
-//            while (num % 2 == 1) {
-//                // sum = sum + i;
-//                System.out.println(num);
-//                sum += num;
-//            }
-//        }
-//        System.out.println("Sum = " + sum);
-//    }
