c4a8a61f513f3e98b456833a5104fb0593238234
dastan <dosyafree32@gmail.com>
2023-02-13 14:57:18 +0600

    I don't know what I'm commit

--- src/Solution.java
+++ src/Solution.java
@@ -1,18 +1,6 @@
-import java.util.Scanner;
-
 public class Solution {
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int count = 0;
-        int[] arr = new int[sc.nextInt()];
-        for (int i = 0; i < arr.length; i++) {
-            i = sc.nextInt();
-        }
-        for (int i = 0; i < arr.length; i++) {
-            for (int j = 0; j > arr[i] / 2; j++) {
 
-            }
-        }
     }
 }
