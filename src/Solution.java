9acab2f48c13d82ddb28a8918bf29bb77dbef083
dastan <dosyafree32@gmail.com>
2023-02-17 14:59:56 +0600

    I'm create game

--- src/Solution.java
+++ src/Solution.java
@@ -1,6 +1,31 @@
+import java.util.Random;
+import java.util.Scanner;
+
 public class Solution {
 
     public static void main(String[] args) {
-
+        Scanner sc = new Scanner(System.in);
+        method(sc);
+    }
+    public static void method(Scanner sc){
+        Random ran = new Random();
+        int a = ran.nextInt(100);
+        int count = 0;
+        System.out.print("Enter to number:");
+        int b = sc.nextInt();
+        while (b != a){
+            if (b < a){
+                System.out.println("your number is small");
+            count++;
+            b = sc.nextInt();
+            } else if (b > a) {
+                System.out.println("your number is big");
+                count++;
+                b = sc.nextInt();
+            }
+        }
+        if (b == a){
+            System.out.println("You are win!!!\nYou are using "+count+" attempt\nright number:"+a);
+        }
     }
 }
